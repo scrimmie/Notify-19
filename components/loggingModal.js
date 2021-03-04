8615037c20899d975485d9ab61d9fd969aad70b2
David D'Apice <48042051+scrimmie@users.noreply.github.com>
2021-03-04 01:41:26 -0500

    Added in test results for modal
    Modal now will have a checkbox for testing and have an upload for test results rendered conditionally.

--- components/loggingModal.js
+++ components/loggingModal.js
@@ -1,11 +1,9 @@
 import Select from "react-select";
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function Modal({ modalClick, date }) {
   const options = [
@@ -52,9 +50,10 @@ export default function Modal({ modalClick, date }) {
   };
 
   const [fields, setFields] = useState([{ Location: null }]);
+  const [positive, setPositive] = useState(false);
+
 
   function handleChange(i, selected, key) {
-    
     const values = [...fields];
     values[i][key] = selected;
     setFields(values);
@@ -77,7 +76,7 @@ export default function Modal({ modalClick, date }) {
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-100	 bg-opacity-95	outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-100	 bg-opacity-95	outline-none focus:outline-none overflow-y-auto max-h-screen">
             {/*header*/}
             <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
               <h3 className="text-3xl font-semibold">
@@ -98,44 +97,68 @@ export default function Modal({ modalClick, date }) {
               {fields.map((field, idx) => {
                 return (
                   <div
-                    className="grid grid-cols-6 grid-rows-3 p-5 grid-flow-row "
+                    className="grid grid-cols-6 grid-rows-2 p-5 grid-flow-row "
                     key={`${field}-${idx}`}
                   >
-                    <div key={`${field}-${idx}`} className="col-span-2 px-4 self-center">
-                    <p className="pl-1">Location</p>
+                    <div
+                      key={`${field}-${idx}`}
+                      className="col-span-2 px-4 self-center"
+                    >
+                      <p className="pl-1">Location</p>
                       <Select
                         options={options}
                         key={`${field}-${idx}`}
-                        onChange={(selected) => handleChange(idx, selected.value, "Location")}
+                        onChange={(selected) =>
+                          handleChange(idx, selected.value, "Location")
+                        }
                       />
                     </div>
                     <div className="justify-self-center self-center">
-                    <p className="pl-1">Time-In</p>
+                      <p className="pl-1">Time-In</p>
                       <input
                         type="time"
                         placeholder="Time-In"
-                        onChange={(selected) => handleChange(idx, selected.target.value, "Time-In")}
+                        onChange={(selected) =>
+                          handleChange(idx, selected.target.value, "Time-In")
+                        }
                         className="pl-6 pr-4 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
                       />
                     </div>
-                    <FontAwesomeIcon className="h-7 mt-5 text-black cursor-pointer leading-none border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none justify-self-center self-center" icon={faArrowRight}/>
+                    <FontAwesomeIcon
+                      className="h-7 mt-5 text-black cursor-pointer leading-none border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none justify-self-center self-center"
+                      icon={faArrowRight}
+                    />
                     <div className=" justify-self-center self-center">
                       <p className="pl-1">Time-Out</p>
                       <input
                         type="time"
                         placeholder="Time-Out"
-                        onChange={(selected) => handleChange(idx, selected.target.value, "Time-Out")}
+                        onChange={(selected) =>
+                          handleChange(idx, selected.target.value, "Time-Out")
+                        }
                         className="pl-6 pr-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
                       />
                     </div>
-                    <button className="justify-self-center self-center" onClick={() => handleRemove(idx)}>
-                      <FontAwesomeIcon className="h-7 text-black cursor-pointer leading-none pr-3 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none " icon={faTimesCircle}/>
+                    <button
+                      className="justify-self-center self-center"
+                      onClick={() => handleRemove(idx)}
+                    >
+                      <FontAwesomeIcon
+                        className="h-7 text-black cursor-pointer leading-none pr-3 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none "
+                        icon={faTimesCircle}
+                      />
                     </button>
-                    <div className="col-span-6 ml-4 mr-4 my-3 row-span-2">
+                    <div className="col-span-6 ml-4 mr-4 my-3 row-span-1">
                       <input
                         type="text"
                         placeholder="Description"
-                        onChange={(selected) => handleChange(idx, selected.target.value, "Description")}
+                        onChange={(selected) =>
+                          handleChange(
+                            idx,
+                            selected.target.value,
+                            "Description"
+                          )
+                        }
                         className="px-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full h-full"
                       />
                     </div>
@@ -143,9 +166,55 @@ export default function Modal({ modalClick, date }) {
                 );
               })}
               <div className="mx-auto flex">
-              <button className="mx-auto my-4" onClick={() => handleAdd()}>
-                <FontAwesomeIcon className="h-8 text-black cursor-pointer leading-none border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none" icon={faPlus}/>
-              </button>
+                <label className="inline-flex items-center mt-3 mx-auto">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox h-5 w-5 text-blue-600"
+                    onClick={() => positive? setPositive(false):setPositive(true)}
+                  />
+                  <span className="ml-2 text-gray-700">
+                    I have shown symptoms or tested positive for COVID-19
+                  </span>
+                </label>
+              </div>
+              {positive?
+              <div className="mx-auto flex">
+                <div className="w-64 mt-4 mb-4 mx-auto">
+                  <button className="bg-blue hover:bg-blue-light text-black font-bold py-2 px-4 w-full inline-flex items-center">
+                    <svg
+                      fill="#000"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      width="18"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
+                    </svg>
+                    <span className="ml-2">Upload Test Results</span>
+                    <input
+                    className="cursor-pointer absolute block py-2 px-4 w-full opacity-0 pin-r pin-t"
+                    type="file"
+                    name="documents[]"
+                    accept="image/*"
+                  />
+                  </button>
+                  
+                </div>
+              </div>:
+              <></>
+              }
+              <div className="mx-auto flex">
+                <button
+                  className="mx-auto my-4 grid grid-cols-1 grid-rows-2 justify-items-center"
+                  onClick={() => handleAdd()}
+                >
+                  <FontAwesomeIcon
+                    className="h-8 text-black cursor-pointer leading-none border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none"
+                    icon={faPlus}
+                  />
+                  <p>Add New Location</p>
+                </button>
               </div>
             </div>
             {/*footer*/}
