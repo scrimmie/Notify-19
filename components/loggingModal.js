c792be69a03e3cbd597394ec7d3af74d4e1ba568
David D'Apice <48042051+scrimmie@users.noreply.github.com>
2021-03-04 00:42:32 -0500

    finished modal implementatio
    modal now stores data for each location and returns them as an object in state (TODO: make mobile friendly)

--- components/loggingModal.js
+++ components/loggingModal.js
@@ -1,5 +1,47 @@
-export default function Modal({ modalClick, date }) {
+import Select from "react-select";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
+
+
+export default function Modal({ modalClick, date }) {
+  const options = [
+    { value: "Hagerty Library", label: "Hagerty Library", key: 1 },
+    {
+      value: "Center for Automation Technology",
+      label: "Center for Automation Technology",
+      key: 2,
+    },
+    { value: "Chestnut Square", label: "Chestnut Square", key: 3 },
+    { value: "Creese Student Center", label: "Creese Student Center", key: 4 },
+    { value: "Disque Hall", label: "Disque Hall", key: 5 },
+    {
+      value: "Drexel Recreation Center",
+      label: "Drexel Recreation Center",
+      key: 6,
+    },
+    {
+      value: "Library Learning Terrace",
+      label: "Library Learning Terrace",
+      key: 7,
+    },
+    {
+      value: "Northside Dining Terrace",
+      label: "Northside Dining Terrace",
+      key: 8,
+    },
+    {
+      value: "Pearlstein Business Learning Center",
+      label: "Pearlstein Business Learning Center",
+      key: 9,
+    },
+    { value: "Randell Hall", label: "Randell Hall", key: 10 },
+    { value: "University Crossings", label: "University Crossings", key: 11 },
+    { value: "URBN Center", label: "URBN Center", key: 12 },
+  ];
 
   const setShowModal = ({ out }) => {
     modalClick(out);
@@ -9,15 +51,33 @@ export default function Modal({ modalClick, date }) {
     //store the user data
   };
 
+  const [fields, setFields] = useState([{ Location: null }]);
+
+  function handleChange(i, selected, key) {
+    
+    const values = [...fields];
+    values[i][key] = selected;
+    setFields(values);
+  }
+
+  function handleAdd() {
+    const values = [...fields];
+    values.push({ Location: null });
+    setFields(values);
+  }
+
+  function handleRemove(i) {
+    const values = [...fields];
+    values.splice(i, 1);
+    setFields(values);
+  }
+
   return (
     <>
-      <div
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-        onClick={() => setShowModal(false)}
-      >
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-100	 bg-opacity-95	outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
               <h3 className="text-3xl font-semibold">
@@ -28,19 +88,65 @@ export default function Modal({ modalClick, date }) {
                   "/" +
                   date.getFullYear()}
               </h3>
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={() => setShowModal(false)}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
+              <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                ×
+              </span>
             </div>
             {/*body*/}
-            <div className="relative p-6 flex-auto">
+            <div className="">
               {/* Data Input */}
-
+              {fields.map((field, idx) => {
+                return (
+                  <div
+                    className="grid grid-cols-6 grid-rows-3 p-5 grid-flow-row "
+                    key={`${field}-${idx}`}
+                  >
+                    <div key={`${field}-${idx}`} className="col-span-2 px-4 self-center">
+                    <p className="pl-1">Location</p>
+                      <Select
+                        options={options}
+                        key={`${field}-${idx}`}
+                        onChange={(selected) => handleChange(idx, selected.value, "Location")}
+                      />
+                    </div>
+                    <div className="justify-self-center self-center">
+                    <p className="pl-1">Time-In</p>
+                      <input
+                        type="time"
+                        placeholder="Time-In"
+                        onChange={(selected) => handleChange(idx, selected.target.value, "Time-In")}
+                        className="pl-6 pr-4 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
+                      />
+                    </div>
+                    <FontAwesomeIcon className="h-7 mt-5 text-black cursor-pointer leading-none border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none justify-self-center self-center" icon={faArrowRight}/>
+                    <div className=" justify-self-center self-center">
+                      <p className="pl-1">Time-Out</p>
+                      <input
+                        type="time"
+                        placeholder="Time-Out"
+                        onChange={(selected) => handleChange(idx, selected.target.value, "Time-Out")}
+                        className="pl-6 pr-2 py-2 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"
+                      />
+                    </div>
+                    <button className="justify-self-center self-center" onClick={() => handleRemove(idx)}>
+                      <FontAwesomeIcon className="h-7 text-black cursor-pointer leading-none pr-3 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none " icon={faTimesCircle}/>
+                    </button>
+                    <div className="col-span-6 ml-4 mr-4 my-3 row-span-2">
+                      <input
+                        type="text"
+                        placeholder="Description"
+                        onChange={(selected) => handleChange(idx, selected.target.value, "Description")}
+                        className="px-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full h-full"
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+              <div className="mx-auto flex">
+              <button className="mx-auto my-4" onClick={() => handleAdd()}>
+                <FontAwesomeIcon className="h-8 text-black cursor-pointer leading-none border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none" icon={faPlus}/>
+              </button>
+              </div>
             </div>
             {/*footer*/}
             <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
@@ -53,10 +159,13 @@ export default function Modal({ modalClick, date }) {
                 Close
               </button>
               <button
-                className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                className="bg-blue-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
                 type="button"
                 style={{ transition: "all .15s ease" }}
-                onClick={() => setShowModal(false)}
+                onClick={() => {
+                  setShowModal(false);
+                  console.log(fields);
+                }}
               >
                 Save Changes
               </button>
